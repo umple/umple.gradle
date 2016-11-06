31dc48b12e45e3bb8e68c30ce8135f2a22512376
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2016-11-06 16:18:05 -0700

    - Build script in umple main repo succeeds - Need to figure out how to link to umple.jar

--- src/main/groovy/cruise/umple/UmpleGradlePlugin.groovy
+++ src/main/groovy/cruise/umple/UmpleGradlePlugin.groovy
@@ -1,33 +1,13 @@
 package cruise.umple
 
-import cruise.umple.UmpleConsoleConfig
-
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class UmpleGradlePlugin implements Plugin<Project> {
 
-    private UmpleConsoleConfig cfg
-
     @Override
     void apply(final Project project) {
 
-        project.task('compileUmpleFile') << {
-			UmpleConsoleMain(cfg)
-		
-			if(project.hasProperty("umpleArgs"))
-			{
-				// arguments are specified through gradle by -P, separated by commas (-P is for project properties)
-				// eg: "gradle compileUmpleFile '-PumpleArgs=test.ump,-g,Java'"
-				args(umpleArgs.split(','))
-				
-				UmpleConsoleMain(args)
-				runConsole()
-			}
-			else
-			{
-				println "Error: Command line arguments are required to compile an Umple file"
-			}
-        }
+
     }
 }
