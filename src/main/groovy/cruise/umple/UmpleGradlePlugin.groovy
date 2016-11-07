1f6e7ea4fe3cc44f180b8ad088dabd0fca92c6c0
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2016-11-06 21:59:34 -0700

    - Changes to plugin implementation

--- src/main/groovy/cruise/umple/UmpleGradlePlugin.groovy
+++ src/main/groovy/cruise/umple/UmpleGradlePlugin.groovy
@@ -1,32 +1,31 @@
 package cruise.umple
 
 import cruise.umple.UmpleConsoleConfig
+import cruise.umple.UmpleConsoleMain
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.GradleException
 
 class UmpleGradlePlugin implements Plugin<Project> {
-
-    private UmpleConsoleConfig cfg
 
     @Override
     void apply(final Project project) {
 
         project.task('compileUmpleFile') << {
-			UmpleConsoleMain(cfg)
+			// UmpleConsoleMain(cfg)
 		
-			if(project.hasProperty("umpleArgs"))
+			if(project.hasProperty('umpleArgs'))
 			{
 				// arguments are specified through gradle by -P, separated by commas (-P is for project properties)
-				// eg: "gradle compileUmpleFile '-PumpleArgs=test.ump,-g,Java'"
-				args(umpleArgs.split(','))
-				
-				UmpleConsoleMain(args)
+				// eg: "gradle compileUmpleFile -PumpleArgs=test.ump,-g,Java"	
+				def cfg = new UmpleConsoleConfig("test.ump -g Java") // umpleArgs.split(','))
+				cruise.umple.UmpleConsoleMain.UmpleConsoleMain(cfg)
 				runConsole()
 			}
 			else
 			{
-				println "Error: Command line arguments are required to compile an Umple file"
+				throw new GradleException("Error: Command line arguments are required to compile an Umple file")
 			}
         }
     }
