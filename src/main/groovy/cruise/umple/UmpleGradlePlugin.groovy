8a1e65b605490cfdb8d0ed96a69c9abb52390d94
Adam Bolding-Jones <anb1@sfu.ca>
2017-02-24 15:10:01 -0800

    -Refactored original plugin code to make it easier to view and change plugin property values -Added default values for plugin property values -Added addGeneratedSourceToProject function that causes the plugin add the Java files it generates to the "generatedSource" source set. This source set is defined in the project the plugin was applied to, and is used to compile the generated Java files

--- src/main/groovy/cruise/umple/UmpleGradlePlugin.groovy
+++ src/main/groovy/cruise/umple/UmpleGradlePlugin.groovy
@@ -5,66 +5,62 @@ import cruise.umple.UmpleConsoleMain
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.GradleException
 
 class UmpleGradlePlugin implements Plugin<Project> {
-
+	// Project properties
+	private final String UMPLE_FILE_PATH = 'umpleFileName'
+	private final String LANGUAGE_TO_GENERATE = 'languageToGenerate'
+	private final String GENERATED_OUTPUT_PATH = 'outputPath'
+		
 	// Member variables
-	private String m_umpleFileName = ""
-	private String m_languageToGenerate = ""
-	private String m_outputPath = ""
-	private String m_defaultOutputPath = "umpleOutput/"
-	
+	private String m_languageToGenerate = ''
+	private String m_generatedOutputPath = ''
+	private m_umpleFilePath = ''
 	private UmpleConsoleConfig m_consoleConfig 
 	private UmpleConsoleMain m_consoleMain
 
-    @Override
-    void apply(final Project project) {
-	
-		project.task('compileUmpleFile') << {
-			// command line arguments are specified through gradle by -P (-P is for project properties)
-			// eg: "gradle compileUmpleFile -PumpleFileName=test.ump -PlanguageToGenerate=Java"	
+	@Override
+	void apply(final Project project) { 	
+		project.plugins.apply 'java'
 		
-			if(project.hasProperty('umpleFileName'))
+		// Set default property values
+		m_languageToGenerate = 'Java'
+		m_generatedOutputPath = "${project.buildDir}${File.separator}generated${File.separator}src${File.separator}java"
+		m_umpleFilePath = "${project.projectDir}${File.separator}src${File.separator}master.ump"
+    	
+		project.task('generateSource') << {		
+			// command line arguments can be specified through gradle by -P (-P is for project properties)
+			// eg: "gradle compileUmpleFile -PUMPLE_FILE_PATH=test.ump -PLANGUAGE_TO_GENERATE=Java"	
+		
+			if(project.hasProperty(UMPLE_FILE_PATH))
 			{		
-				m_umpleFileName = project.getProperty('umpleFileName')
-				m_consoleConfig = new UmpleConsoleConfig(m_umpleFileName) 
+				m_umpleFilePath = project.getProperty(UMPLE_FILE_PATH)	
 			}
-			else
-			{
-				throw new GradleException("Error: You must specify an Umple file")
-			}
+			m_consoleConfig = new UmpleConsoleConfig(m_umpleFilePath) 
 			
-			if(project.hasProperty('languageToGenerate'))
-			{				
-				m_languageToGenerate = project.getProperty('languageToGenerate')
-				m_consoleConfig.setGenerate(m_languageToGenerate)
-			}
-			else
-			{
-				if(m_languageToGenerate != "")
-				{
-					m_consoleConfig.setGenerate(m_languageToGenerate)
-				}
-				else
-				{
-					throw new GradleException("Error: You must specify a language to generate code for")
-				}
-			}
 			
-			if(project.hasProperty('outputPath'))
+			if(project.hasProperty(LANGUAGE_TO_GENERATE))
 			{				
-				m_consoleConfig.setPath(project.getProperty('outputPath'))
+				m_languageToGenerate = project.getProperty(LANGUAGE_TO_GENERATE)				
 			}
-			else
-			{		
-				m_consoleConfig.setPath(m_defaultOutputPath)
-			}				
-				
+			m_consoleConfig.setGenerate(m_languageToGenerate)
+			
+			if(project.hasProperty(GENERATED_OUTPUT_PATH))
+			{			
+				m_generatedOutputPath = project.getProperty(GENERATED_OUTPUT_PATH)
+			}		
+			m_consoleConfig.setPath(m_generatedOutputPath)	
+			
 			m_consoleMain = new UmpleConsoleMain(m_consoleConfig)
-				
 			m_consoleMain.runConsole()
-		}
-		
-    }
+			
+			addGeneratedToSource(project)
+		}	 
+  }
+    
+	void addGeneratedToSource(Project project) {
+   project.sourceSets.matching { it.name == "generatedSource" } .all {
+    it.java.srcDir "${project.buildDir}${File.separator}generated${File.separator}src${File.separator}java"
+   }
+ 	}    	
 }
