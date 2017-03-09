f9c901be6ee2e8b9beefdf4b417a01923ffda79e
Adam Bolding-Jones <anb1@sfu.ca>
2017-03-09 15:44:45 -0800

    Tweaking integrationTest/groovy/CompilationAndGeneration.groovy to test the sourceSet property changes in the previous commit. More work could be done here, but I think it's good enough for now.

--- src/integrationTest/groovy/CompilationAndGeneration.groovy
+++ src/integrationTest/groovy/CompilationAndGeneration.groovy
@@ -18,8 +18,8 @@ public class CompilationAndGeneration {
         ImmutableMultimap.Builder<String, ArrayList<String>> builder = ImmutableMultimap.builder();
         builder.put(DIRECTORIES_ROOT + "subproj/build/classes/generatedSource", ["Subproj.class", "Subproj2.class"])
         builder.put(DIRECTORIES_ROOT + "sub2/build/classes/generatedSource", ["Sub2.class", "Sub22.class"])
-        builder.put(DIRECTORIES_ROOT + "subproj/generated/java", ["Subproj.java", "Subproj2.java"])
-        builder.put(DIRECTORIES_ROOT + "sub2/generated/java", ["Sub2.java", "Sub22.java"])
+        builder.put(DIRECTORIES_ROOT + "subproj/customPath/generated/java", ["Subproj.java", "Subproj2.java"])
+        builder.put(DIRECTORIES_ROOT + "sub2/customPath/generated/java", ["Sub2.java", "Sub22.java"])
         DIRECTORIES_TO_CHECK = builder.build();
     }
 
